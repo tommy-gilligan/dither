f74c98bbebb9d3cada4fd3dd6268badb20db59ce
Tommy G <7865781+tommy-gilligan@users.noreply.github.com>
2024-01-09 22:57:30 +1100

    no_std

--- examples/bmp.rs
+++ examples/bmp.rs
@@ -1,20 +1,19 @@
-use embedded_graphics::{
-    image::{Image, ImageRaw},
-    pixelcolor::{Rgb888, RgbColor},
-    prelude::*,
-};
+use embedded_graphics::primitives::Rectangle;
+use embedded_graphics::{pixelcolor::Rgb888, prelude::*};
 use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
-use tinybmp::{Bmp, RawBmp};
+use tinybmp::Bmp;
 
 fn main() -> Result<(), core::convert::Infallible> {
     let bmp: Bmp<Rgb888> = Bmp::from_slice(include_bytes!("./mona_lisa.bmp")).unwrap();
-    let abmp = RawBmp::from_slice(include_bytes!("./mona_lisa.bmp")).unwrap();
-    let size = abmp.header().image_size;
-    let mut display = SimulatorDisplay::<Rgb888>::new(Size::new(size.width, size.height));
+    let mut display = SimulatorDisplay::<Rgb888>::new(bmp.size());
 
-    let iter = dither::dither(bmp.pixels().map(|c| c.1), size, dither::stochastic_closest);
-    let pixels: Vec<u8> = iter.flat_map(|c| [c.r(), c.g(), c.b()]).collect();
-    Image::new(&ImageRaw::<Rgb888>::new(&pixels, size.width), Point::zero()).draw(&mut display)?;
+    display
+        .fill_contiguous(
+            &Rectangle::new(Point::zero(), bmp.size()),
+            dither::Dither::new(bmp.size(), bmp.pixels().map(|c| c.1), dither::closest),
+        )
+        .unwrap();
+
     Window::new("Hello World", &OutputSettingsBuilder::new().build()).show_static(&display);
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+#![no_std]
+
+use core::ops::{Index, IndexMut};
 use embedded_graphics_core::{
     geometry::Size,
     pixelcolor::{Rgb888, RgbColor},
+    prelude::*,
 };
-use rand::prelude::*;
-
+use heapless::Vec;
 use nalgebra::Vector3;
 
 pub fn color_to_vector<C>(color: C) -> Vector3<i32>
@@ -13,7 +16,8 @@ where
     Vector3::<i32>::new(color.r() as i32, color.g() as i32, color.b() as i32)
 }
 
-pub fn vector_to_colora(color: Vector3<i32>) -> Rgb888 {
+// eliminate panic
+pub fn vector_to_color(color: Vector3<i32>) -> Rgb888 {
     Rgb888::new(
         color[0].clamp(0, 255).try_into().unwrap_or(0x00),
         color[1].clamp(0, 255).try_into().unwrap_or(0x00),
@@ -21,59 +25,115 @@ pub fn vector_to_colora(color: Vector3<i32>) -> Rgb888 {
     )
 }
 
-pub fn dither<F, I>(pixels: I, size: Size, f: F) -> impl Iterator<Item = Rgb888>
+pub struct Dither<I, F>
 where
-    F: Fn(Vector3<i32>) -> Vector3<i32>,
     I: Iterator<Item = Rgb888>,
+    F: Fn(Rgb888) -> Rgb888,
 {
-    let pixels_copy_a: Vec<Vector3<i32>> = pixels.map(color_to_vector).collect();
-    let mut pixels_copy: Vec<Vec<Vector3<i32>>> = pixels_copy_a
-        .chunks(size.width.try_into().unwrap())
-        .map(|a| a.to_vec())
-        .collect();
-    let mut peekable = pixels_copy.iter_mut().enumerate().peekable();
+    buffer: WrappingVec,
+    size: Size,
+    source_pixels: I,
+    f: F,
+}
 
-    while let Some((i, row)) = peekable.next() {
-        let row_next = peekable.peek_mut();
+impl<I, F> Dither<I, F>
+where
+    I: Iterator<Item = Rgb888>,
+    F: Fn(Rgb888) -> Rgb888,
+{
+    // should error
+    pub fn new(size: Size, mut source_pixels: I, f: F) -> Self {
+        let v = WrappingVec::new(size, &mut source_pixels);
 
-        for x in 0..(size.width as usize) {
-            let x = if i % 2 == 0 {
-                x
-            } else {
-                size.width as usize - 1 - x
-            };
-            let oldpixel = row[x];
-            let newpixel = f(oldpixel);
-            row[x] = newpixel;
-            let quant_error = oldpixel - newpixel;
-
-            if (x + 1) < (size.width as usize) {
-                row[x + 1] += quant_error * 7 / 16
-            }
-            if (x as isize - 1) >= 0 {
-                if let Some((_, row_next)) = row_next {
-                    row_next[x - 1] += quant_error * 3 / 16
-                }
-            }
-            if let Some((_, row_next)) = row_next {
-                row_next[x] += quant_error * 5 / 16
-            }
-            if (x + 1) < (size.width as usize) {
-                if let Some((_, row_next)) = row_next {
-                    row_next[x + 1] += quant_error * 1 / 16
-                }
-            }
+        Self {
+            buffer: v,
+            size,
+            f,
+            source_pixels,
         }
     }
+}
 
-    pixels_copy
-        .into_iter()
-        .flat_map(|row| row.into_iter().map(vector_to_colora))
+struct WrappingVec {
+    v: Vec<Vector3<i32>, 1024>,
+    cursor: usize,
+}
+
+impl WrappingVec {
+    fn new<I>(size: Size, source_pixels: &mut I) -> Self
+    where
+        I: Iterator<Item = Rgb888>,
+    {
+        let mut v = Vec::new();
+        for _ in 0..(size.width + 1) {
+            v.push(color_to_vector(source_pixels.next().unwrap()))
+                .unwrap();
+        }
+
+        Self { v, cursor: 0 }
+    }
+
+    fn push(&mut self, item: Vector3<i32>) {
+        self.v[self.cursor] = item;
+        self.cursor = (self.cursor + 1) % self.v.len();
+    }
+}
+
+impl Index<usize> for WrappingVec {
+    type Output = Vector3<i32>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.v[(self.cursor + index) % self.v.len()]
+    }
+}
+
+impl IndexMut<usize> for WrappingVec {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        let len = self.v.len();
+        &mut self.v[(self.cursor + index) % len]
+    }
+}
+
+impl<I, F> Iterator for Dither<I, F>
+where
+    I: Iterator<Item = Rgb888>,
+    F: Fn(Rgb888) -> Rgb888,
+{
+    type Item = Rgb888;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_pixel) = self.source_pixels.next() {
+            let oldpixel = self.buffer[0];
+            let newpixel = (self.f)(vector_to_color(oldpixel));
+            let quant_error = oldpixel - color_to_vector(newpixel);
+
+            self.buffer[1] += (quant_error * 7) / 16;
+            self.buffer[self.size.width as usize - 1] += (quant_error * 3) / 16;
+            self.buffer[self.size.width as usize] += (quant_error * 5) / 16;
+            self.buffer[self.size.width as usize + 1] += quant_error / 16;
+
+            self.buffer.push(color_to_vector(next_pixel));
+
+            Some(newpixel)
+        } else {
+            None
+        }
+    }
+}
+
+impl<I, F> OriginDimensions for Dither<I, F>
+where
+    I: Iterator<Item = Rgb888>,
+    F: Fn(Rgb888) -> Rgb888,
+{
+    fn size(&self) -> Size {
+        self.size
+    }
 }
 
 #[test]
 fn test_dither() {
-    let mut image: Vec<Rgb888> = vec![
+    let mut image: [Rgb888; 9] = vec![
         Rgb888::new(0xcc, 0xcc, 0xcc),
         Rgb888::new(0xcc, 0xcc, 0xcc),
         Rgb888::new(0xcc, 0xcc, 0xcc),
@@ -84,11 +144,11 @@ fn test_dither() {
         Rgb888::new(0xcc, 0xcc, 0xcc),
         Rgb888::new(0xcc, 0xcc, 0xcc),
     ];
-    let output: Vec<_> = dither(image.into_iter(), Size::new(3, 3), closest).collect();
+    let output: [Rgb888; 9] = dither(image.into_iter(), Size::new(3, 3), closest).collect();
 
     assert_eq!(
         output,
-        vec![
+        [
             Rgb888::new(170, 170, 170),
             Rgb888::new(170, 170, 170),
             Rgb888::new(170, 170, 170),
@@ -102,30 +162,31 @@ fn test_dither() {
     )
 }
 
-const CGA_PALETTE: [Vector3<i32>; 14] = [
-    Vector3::<i32>::new(0x00, 0x00, 0x00),
-    Vector3::<i32>::new(0x00, 0x00, 0xAA),
-    Vector3::<i32>::new(0x00, 0xAA, 0x00),
-    Vector3::<i32>::new(0x00, 0xAA, 0xAA),
-    Vector3::<i32>::new(0xAA, 0x00, 0x00),
-    Vector3::<i32>::new(0xAA, 0x00, 0xAA),
-    Vector3::<i32>::new(0xAA, 0xAA, 0xAA),
-    Vector3::<i32>::new(0xAA, 0x55, 0x00),
-    Vector3::<i32>::new(0xff, 0xff, 0xff),
-    Vector3::<i32>::new(0x55, 0x55, 0x55),
-    Vector3::<i32>::new(0x55, 0x55, 0xFF),
-    Vector3::<i32>::new(0xFF, 0xFF, 0x55),
-    Vector3::<i32>::new(0xFF, 0x55, 0x55),
-    Vector3::<i32>::new(0x55, 0xFF, 0x55),
+const CGA_PALETTE: [Rgb888; 14] = [
+    Rgb888::new(0x00, 0x00, 0x00),
+    Rgb888::new(0x00, 0x00, 0xAA),
+    Rgb888::new(0x00, 0xAA, 0x00),
+    Rgb888::new(0x00, 0xAA, 0xAA),
+    Rgb888::new(0xAA, 0x00, 0x00),
+    Rgb888::new(0xAA, 0x00, 0xAA),
+    Rgb888::new(0xAA, 0xAA, 0xAA),
+    Rgb888::new(0xAA, 0x55, 0x00),
+    Rgb888::new(0xff, 0xff, 0xff),
+    Rgb888::new(0x55, 0x55, 0x55),
+    Rgb888::new(0x55, 0x55, 0xFF),
+    Rgb888::new(0xFF, 0xFF, 0x55),
+    Rgb888::new(0xFF, 0x55, 0x55),
+    Rgb888::new(0x55, 0xFF, 0x55),
 ];
 
-pub fn closest(pixel: Vector3<i32>) -> Vector3<i32> {
+// eliminate panic
+pub fn closest(pixel: Rgb888) -> Rgb888 {
     CGA_PALETTE
         .into_iter()
         .min_by_key(|cga| {
-            let r = pixel[0] - cga[0];
-            let g = pixel[1] - cga[1];
-            let b = pixel[2] - cga[2];
+            let r = pixel.r().abs_diff(cga.r()) as u32;
+            let g = pixel.g().abs_diff(cga.g()) as u32;
+            let b = pixel.b().abs_diff(cga.b()) as u32;
 
             r * r + g * g + b * b
         })
@@ -138,19 +199,4 @@ fn distance(pixel: Vector3<i32>, cga: Vector3<i32>) -> i32 {
     let b = pixel[2] - cga[2];
 
     r * r + g * g + b * b
-}
-
-const THRESHOLD: i32 = 1000;
-
-pub fn stochastic_closest(pixel: Vector3<i32>) -> Vector3<i32> {
-    let mut palette = CGA_PALETTE.to_vec();
-    palette.sort_by_key(|cga| distance(pixel, *cga));
-    let mut filtered: Vec<Vector3<i32>> = palette.clone().into_iter().filter(|cga| distance(pixel, *cga) < THRESHOLD).collect();
-    let mut rng = rand::thread_rng();
-    filtered.shuffle(&mut rng);
-    if filtered.len() > 0 {
-        filtered[0]
-    } else {
-        palette[0]
-    }
 }
