c4f8ff3463207cf0d93c006244808e61734474c5
Tommy Gilligan <7865781+tommy-gilligan@users.noreply.github.com>
2024-01-14 01:25:58 +1100

    remove default palette

--- examples/bmp.rs
+++ examples/bmp.rs
@@ -18,3 +18,33 @@ fn main() -> Result<(), core::convert::Infallible> {
 
     Ok(())
 }
+
+const CGA_PALETTE: [Rgb888; 14] = [
+    Rgb888::new(0x00, 0x00, 0x00),
+    Rgb888::new(0x00, 0x00, 0xAA),
+    Rgb888::new(0x00, 0xAA, 0x00),
+    Rgb888::new(0x00, 0xAA, 0xAA),
+    Rgb888::new(0xAA, 0x00, 0x00),
+    Rgb888::new(0xAA, 0x00, 0xAA),
+    Rgb888::new(0xAA, 0xAA, 0xAA),
+    Rgb888::new(0xAA, 0x55, 0x00),
+    Rgb888::new(0xff, 0xff, 0xff),
+    Rgb888::new(0x55, 0x55, 0x55),
+    Rgb888::new(0x55, 0x55, 0xFF),
+    Rgb888::new(0xFF, 0xFF, 0x55),
+    Rgb888::new(0xFF, 0x55, 0x55),
+    Rgb888::new(0x55, 0xFF, 0x55),
+];
+
+pub fn closest(pixel: Rgb888) -> Rgb888 {
+    CGA_PALETTE
+        .into_iter()
+        .min_by_key(|cga| {
+            let r = pixel.r().abs_diff(cga.r()) as u32;
+            let g = pixel.g().abs_diff(cga.g()) as u32;
+            let b = pixel.b().abs_diff(cga.b()) as u32;
+
+            r * r + g * g + b * b
+        })
+        .unwrap()
+}
