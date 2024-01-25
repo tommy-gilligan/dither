1d7b551c57656e599eb8f67cccc1ecdc0aac16d3
Tommy Gilligan <7865781+tommy-gilligan@users.noreply.github.com>
2024-01-25 16:49:15 +1100

    increase use of generics for color conversion

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@ Rgb888: From<<Display as DrawTarget>::Color>
     }
 }
 
-fn vector_from_rgb(rgb: Rgb888) -> Vector3<i16> {
+fn vector_from_rgb<C>(rgb: C) -> Vector3<i16> where C: RgbColor {
     Vector3::<i16>::new(
        rgb.r().into(),
        rgb.g().into(),
@@ -66,14 +66,14 @@ Rgb888: From<<Display as DrawTarget>::Color>
     fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
        where I: IntoIterator<Item = Pixel<Self::Color>>
    {
-         let mut vectors = pixels.into_iter().map(|pixel| vector_from_rgb(pixel.1));
+         let mut vectors = pixels.into_iter().map(|pixel| vector_from_rgb::<Rgb888>(pixel.1));
          let mut buffer: crate::wrapping_vec::WrappingVec<Vector3<i16>, WIDTH_PLUS_ONE> = crate::wrapping_vec::WrappingVec::new(&mut vectors);
 
          self.display.fill_contiguous(
              &Rectangle::new(Point::zero(), self.size()),
              vectors.map(|vector| {
                 let closest_color: Display::Color = rgb_from_vector(buffer[0]).into();
-                let quant_error = buffer[0] - vector_from_rgb(closest_color.into());
+                let quant_error = buffer[0] - vector_from_rgb::<Rgb888>(closest_color.into());
 
                 buffer[1] += (quant_error * 7) / 16;
                 buffer[WIDTH_PLUS_ONE - 2] += (quant_error * 3) / 16;
