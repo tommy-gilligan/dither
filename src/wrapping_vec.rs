fe4b402e1461c057652c76775ad708287448c7bc
Tommy Gilligan <7865781+tommy-gilligan@users.noreply.github.com>
2024-02-04 18:11:20 +1100

    have wrapping &conversion function

--- src/wrapping_vec.rs
+++ src/wrapping_vec.rs
@@ -5,27 +5,31 @@ extern crate std;
 // this is a mechanism to hold N+1 without resorting to unstable const expr
 // complicates logic but at least it's somewhat hidden/isolated
 #[derive(Debug, PartialEq)]
-pub struct WrappingVec<'a, X, I, const N: usize>
+pub struct WrappingVec<'a, X, I, F, Y, const N: usize>
 where
     X: Default + Copy + AddAssign + core::ops::Mul<Output = X> + core::ops::Div<Output = X>,
-    I: Iterator<Item = X>
+    I: Iterator<Item = X>,
+    F: Fn(X) -> (Y, X)
 {
     v: [X; N],
     v_n: X,
     write_index: usize,
     read_index: usize,
     kernel: &'a [((isize, isize), (X, X))],
-    source_pixels: I
+    source_pixels: I,
+    conversion: F
 }
 
-impl<'a, X, I, const N: usize> WrappingVec<'a, X, I, N>
+impl<'a, X, I, F, Y, const N: usize> WrappingVec<'a, X, I, F, Y, N>
 where
     X: Default + Copy + AddAssign + core::ops::Mul<Output = X> + core::ops::Div<Output = X>,
-    I: Iterator<Item = X>
+    I: Iterator<Item = X>,
+    F: Fn(X) -> (Y, X)
 {
     pub fn new(
         mut source_pixels: I,
         kernel: &'a [((isize, isize), (X, X))],
+        conversion: F
     ) -> Self
     where
         I: Iterator<Item = X>,
@@ -37,27 +41,12 @@ where
         }
         let v_n: X = source_pixels.next().unwrap_or_default();
 
-        Self { v, v_n, read_index: 0, write_index: 0, kernel, source_pixels }
+        Self { v, v_n, read_index: 0, write_index: 0, kernel, source_pixels, conversion }
     }
 
-    fn push(&mut self, item: X) {
-        let i = self.write_index;
-
-        self.write_index = if self.write_index < N {
-            self.write_index + 1
-        } else {
-            0
-        };
-
-        if i == N {
-            self.v_n = item;
-        } else {
-            self.v[i] = item;
-        }
-    }
-
-    pub fn apply_kernel(&mut self, error: X) {
+    pub fn apply_kernel(&mut self) {
         let horizon = self.source_pixels.next().unwrap();
+        let error = (self.conversion)(horizon).1;
         for ((x_offset, y_offset), (mul, div)) in self.kernel {
             let index = (self.read_index as isize + x_offset + y_offset * N as isize) % (N as isize + 1);
             // std::println!("{:?}         {:?}   {:?}", self.read_index, x_offset, y_offset);
@@ -71,14 +60,27 @@ where
             }
         }
 
-        self.push(horizon);
+        let i = self.write_index;
+
+        self.write_index = if self.write_index < N {
+            self.write_index + 1
+        } else {
+            0
+        };
+
+        if i == N {
+            self.v_n = horizon;
+        } else {
+            self.v[i] = horizon;
+        }
     }
 }
 
-impl <'a, X, I, const N: usize> Iterator for WrappingVec<'a, X, I, N>
+impl <'a, X, I, F, Y, const N: usize> Iterator for WrappingVec<'a, X, I, F, Y, N>
 where
     X: Default + Copy + AddAssign + core::ops::Mul<Output = X> + core::ops::Div<Output = X>,
-    I: Iterator<Item = X>
+    I: Iterator<Item = X>,
+    F: Fn(X) -> (Y, X)
 {
     type Item = X;
 
@@ -99,33 +101,35 @@ where
     }
 }
 
-#[test]
-fn test_wrapping_vec() {
-    let a: [i16; 6] = [1, 2, 3, 4, 5, 6];
-    let iter = a.into_iter();
-    let mut wv: WrappingVec<i16, _, 4> = WrappingVec::new(iter, &[]);
+const GREYS: [i16; 5] = [
+    0x00,
+    0x40,
+    0x80,
+    0xC0,
+    0xF0
+];
 
-    assert_eq!(wv.next(), Some(1));
-    assert_eq!(wv.next(), Some(2));
-    assert_eq!(wv.next(), Some(3));
-    assert_eq!(wv.next(), Some(4));
-    assert_eq!(wv.next(), Some(5));
-    assert_eq!(wv.next(), Some(1));
+fn quantize(a: i16) -> (i16, i16) {
+    let value = GREYS.into_iter().min_by_key(|ai| {
+        (*ai).abs_diff(a)
+    }).unwrap();
 
-    wv.push(9);
-    assert_eq!(wv.next(), Some(2));
-    assert_eq!(wv.next(), Some(3));
-    assert_eq!(wv.next(), Some(4));
-    assert_eq!(wv.next(), Some(5));
-    assert_eq!(wv.next(), Some(9));
-    assert_eq!(wv.next(), Some(2));
+    (value, a as i16 - value as i16)
+}
+
+fn quantize_ten(a: i16) -> (i16, i16) {
+    let value = GREYS.into_iter().min_by_key(|ai| {
+        (*ai).abs_diff(a)
+    }).unwrap();
+
+    (value, 10)
 }
 
 #[test]
 fn test_wrapping_vec_convolution() {
     let a: [i16; 6] = [1, 2, 3, 4, 5, 6];
     let iter = a.into_iter();
-    let mut wv: WrappingVec<i16, _, 4> = WrappingVec::new(iter, &[((1, 0), (2, 1))]);
+    let mut wv: WrappingVec<i16, _, _, _, 4> = WrappingVec::new(iter, &[((1, 0), (2, 1))], &quantize_ten);
 
     assert_eq!(wv.next(), Some(1));
     assert_eq!(wv.next(), Some(2));
@@ -134,7 +138,7 @@ fn test_wrapping_vec_convolution() {
     assert_eq!(wv.next(), Some(5));
     assert_eq!(wv.next(), Some(1));
 
-    wv.apply_kernel(10);
+    wv.apply_kernel();
 
     assert_eq!(wv.next(), Some(2));
     assert_eq!(wv.next(), Some(23));
@@ -152,26 +156,27 @@ fn test_wrapping_vec_x_range() {
         9, 10,
     ];
     let iter = a.into_iter();
-    let mut wv: WrappingVec<i16, _, 4> = WrappingVec::new(
+    let mut wv: WrappingVec<i16, _, _, _, 4> = WrappingVec::new(
         iter,
         &[
             ((0, 0), (7, 16)),
             ((4, 0), (7, 16)),
             ((-4, 0), (7, 16)),
-        ]
+        ],
+        &quantize_ten
     );
 
     assert_eq!(wv.next(), Some(1));
-    wv.apply_kernel(10);
+    wv.apply_kernel();
 
     assert_eq!(wv.next(), Some(6));
-    wv.apply_kernel(10);
+    wv.apply_kernel();
 
     assert_eq!(wv.next(), Some(7));
-    wv.apply_kernel(10);
+    wv.apply_kernel();
 
     assert_eq!(wv.next(), Some(8));
-    wv.apply_kernel(10);
+    wv.apply_kernel();
 }
 
 #[test]
@@ -182,15 +187,16 @@ fn test_wrapping_vec_y_range() {
         9, 10,
     ];
     let iter = a.into_iter();
-    let mut wv: WrappingVec<i16, _, 4> = WrappingVec::new(
+    let mut wv: WrappingVec<i16, _, _, _, 4> = WrappingVec::new(
         iter,
         &[
             ((0, 1), (7, 16)),
-        ]
+        ],
+        &quantize_ten
     );
 
     assert_eq!(wv.next(), Some(1));
-    std::println!("{:?}", wv);
+    // std::println!("{:?}", wv);
     // wv.apply_kernel(10, iter.next().unwrap());
     // std::println!("{:?}", wv);
 
